87427380a49f243d3beaef65ed73ef87892e0aa1
vinobalan-kannan <vinobalan.kannan@gmail.com>
2026-08-16 13:16:42 +0530

    Testng

--- src/testNG/Try1.java
+++ src/testNG/Try1.java
@@ -1,25 +1,67 @@
 package testNG;
 
-import java.util.Arrays;
-
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Try1 {
 
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("Im B4 suite");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Im B4 Test");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Im B4 class");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Im B4 Method");
+	}
+	@BeforeMethod
+	public void beforeMethod2() {
+		System.out.println("Im 2nd B4 Method");
+	}
+
 	@Test
-	public void Demo() {
-	//	System.out.println("Demo");
-		  
-		        int[] numbers = {10, 20, 30, 40};
-		        
-		        // Converts the array and prints: [10, 20, 30, 40]
-		        System.out.println(Arrays.toString(numbers)); 
-		        
-		        System.out.println("GIT updation");
-		        
-		        System.out.println("checking branch name in workspace");
-		        
-		        System.out.println("checking new branch");
-		    
+	public void test1() {
+		System.out.println("Im 1st test");
+	}
+	@Test
+	public void test2() {
+		System.out.println("Im 2nd test");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("Im after method");
+	}
+
+	@AfterClass
+	public void afterCLass() {
+		System.out.println("Im after class");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		System.out.println("Im after Test");
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("Im after suite");
 	}
 }
---
+++ src/testNG/Try2.java
@@ -0,0 +1,10 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class Try2 {
+	@Test
+	public void test3() {
+		System.out.println("Im 3rd test");
+	}
+}
